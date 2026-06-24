b030cf91fdf725e8ac6241c6cf69d2eccaa19512
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2026-06-23 21:11:11 -0300

    construção das matrizes de massa e rigidez individuais e globais

--- main/atv1_Porticos.py
+++ main/atv1_Porticos.py
@@ -1,6 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+import os
+
+results_dir = "resultados"
+
+def salvar_grafico(plt, nome_pasta, nome_arquivo):
+    os.makedirs(nome_pasta, exist_ok=True)
+    caminho_completo = os.path.join(nome_pasta, nome_arquivo)
+    plt.savefig(caminho_completo, dpi=300, bbox_inches='tight')
+    print(f"-> Gráfico salvo com sucesso em: {caminho_completo}")
+
+def salvar_matriz(M, nome_pasta, nome_base="estrutura"):
+    os.makedirs(nome_pasta, exist_ok=True)
+    caminho_txt = os.path.join(nome_pasta, f"M_{nome_base}.txt")
+    np.savetxt(caminho_txt, M, fmt='%.4e', delimiter='\t')
+    print(f"-> Matriz {nome_base} (.txt) salva em: {caminho_txt}")
 
 class Portico():
     def __init__(self, p1, p2, id):
@@ -17,10 +32,69 @@ class Portico():
         self.I = (np.pi / 4) * (self.r_out**4 - self.r_in**4) # m^4
         self.m = self.rho * self.A * self.L # kg
         self.peso = self.m * 9.81 # N
+        self.k_matriz = self.build_matriz_rigidez()
+        self.m_matriz = self.build_matriz_massa()
     
     def __str__(self):
         return f"Portico {self.id}: p1={self.p1}, p2={self.p2}, L={self.L:.2f} m, Theta={np.degrees(self.theta):.2f}°"
-
+    
+    def build_matriz_rigidez(self):
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        L = self.L
+        
+        T = np.array([
+            [ c,  s,  0,  0,  0,  0],
+            [-s,  c,  0,  0,  0,  0],
+            [ 0,  0,  1,  0,  0,  0],
+            [ 0,  0,  0,  c,  s,  0],
+            [ 0,  0,  0, -s,  c,  0],
+            [ 0,  0,  0,  0,  0,  1]
+        ])
+        
+        EA_L = (self.E * self.A) / L
+        EI_L3 = (self.E * self.I) / (L**3)
+        EI_L2 = (self.E * self.I) / (L**2)
+        EI_L  = (self.E * self.I) / L
+        
+        K_local = np.array([
+            [ EA_L,     0,          0,       -EA_L,     0,          0      ],
+            [   0,   12*EI_L3,   6*EI_L2,       0,  -12*EI_L3,   6*EI_L2   ],
+            [   0,    6*EI_L2,   4*EI_L,        0,   -6*EI_L2,   2*EI_L    ],
+            [-EA_L,     0,          0,        EA_L,     0,          0      ],
+            [   0,  -12*EI_L3,  -6*EI_L2,       0,   12*EI_L3,  -6*EI_L2   ],
+            [   0,    6*EI_L2,   2*EI_L,        0,   -6*EI_L2,   4*EI_L    ]
+        ])
+        
+        return T.T @ K_local @ T
+    
+    def build_matriz_massa(self):
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        L = self.L
+        
+        T = np.array([
+            [ c,  s,  0,  0,  0,  0],
+            [-s,  c,  0,  0,  0,  0],
+            [ 0,  0,  1,  0,  0,  0],
+            [ 0,  0,  0,  c,  s,  0],
+            [ 0,  0,  0, -s,  c,  0],
+            [ 0,  0,  0,  0,  0,  1]
+        ])
+        
+        M_total = self.m
+        
+        M_local = (M_total / 420) * np.array([
+            [420*(2/6),    0,       0,      420*(1/6),    0,       0     ],
+            [ 0,   156,    22*L,       0,   54,   -13*L     ],
+            [ 0,  22*L,  4*L**2,       0, 13*L,  -3*L**2    ],
+            [ 420*(1/6),    0,       0,     420*(2/6),    0,       0     ],
+            [ 0,    54,    13*L,       0,  156,   -22*L     ],
+            [ 0, -13*L, -3*L**2,       0,-22*L,  4*L**2     ]
+        ])
+        
+        return T.T @ M_local @ T
+    
 class Trelica():
     def __init__(self, p1, p2, id):
         self.r = 0.012 # m
@@ -35,9 +109,30 @@ class Trelica():
         self.I = (np.pi / 4) * self.r**4  # m^4
         self.m = self.rho * self.A * self.L # kg
         self.peso = self.m * 9.81 # N
+        self.k_matriz = self.build_matriz_rigidez()
+        self.m_matriz = self.build_matriz_massa()
 
     def __str__(self):
         return f"Trelica {self.id}: p1={self.p1}, p2={self.p2}, L={self.L:.2f} m, Theta={np.degrees(self.theta):.2f}°"
+
+    def build_matriz_rigidez(self):
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        k = (self.E * self.A) / self.L
+        k_matrix = k * np.array([[c**2, c*s, -c**2, -c*s],
+                                  [c*s, s**2, -c*s, -s**2],
+                                  [-c**2, -c*s, c**2, c*s],
+                                  [-c*s, -s**2, c*s, s**2]])
+        return k_matrix
+    
+    def build_matriz_massa(self):
+        c = np.cos(self.theta)
+        s = np.sin(self.theta)
+        m_matriz = (self.m / 6) * np.array([[2*c**2, 2*c*s, c**2, c*s],
+                                            [2*c*s, 2*s**2, c*s, s**2],
+                                            [c**2, c*s, 2*c**2, c*s],
+                                            [c*s, s**2, c*s, 2*s**2]])
+        return m_matriz
 
 class Estrutura():
     def __init__(self):
@@ -45,7 +140,11 @@ class Estrutura():
         self.q = 7500 # N/m
         self.Fw = 2000 # N
         self.f = 1/60 # Hz
+        self.nos = []
+        self.no_para_id = {}
         self.build()
+        self.mapear_nos()
+        self.K_global, self.M_global = self.montar_matrizes_globais()
     
     def build(self):
         self.elementos.append(Portico((0.0, 0.0), (0.0, 1.5), 1))
@@ -79,6 +178,7 @@ class Estrutura():
         self.elementos.append(Portico((4.5, 7.5), (4.5, 9.0), 29))
         self.elementos.append(Trelica((6.0, 7.5), (4.5, 9.0), 30))
         self.elementos.append(Portico((4.5, 7.5), (6.0, 7.5), 31))
+        salvar_grafico(self.plot_estrutura(), results_dir, "EstrturaConstruida.png")
     
     def plot_estrutura(self):
         plt.figure(figsize=(10, 8))
@@ -104,14 +204,70 @@ class Estrutura():
         ticks_y = np.arange(-1.5, 10.5, passo)  # [0.0, 1.5, 3.0, 4.5, 6.0, 7.5, 9.0]
         plt.xticks(ticks_x)
         plt.yticks(ticks_y)
-        plt.axis("equal") 
+        plt.axis("equal")
+        return plt 
+    
+    def mapear_nos(self):
+        conjunto_nos = set()
+        for elem in self.elementos:
+            conjunto_nos.add(elem.p1)
+            conjunto_nos.add(elem.p2)
+            
+        self.nos = sorted(list(conjunto_nos), key=lambda p: (p[1], p[0]))
+        self.no_para_id = {no: idx for idx, no in enumerate(self.nos)}
         
-        plt.show()
-
+    def montar_matrizes_globais(self):
+        num_nos = len(self.nos)
+        num_gdl = 3 * num_nos
+        
+        # Inicializa matrizes globais zeradas
+        K_g = np.zeros((num_gdl, num_gdl))
+        M_g = np.zeros((num_gdl, num_gdl))
+        
+        for elem in self.elementos:
+            id_n1 = self.no_para_id[elem.p1]
+            id_n2 = self.no_para_id[elem.p2]
+            
+            ke = elem.k_matriz
+            me = elem.m_matriz
+            
+            if isinstance(elem, Portico):
+                gdl_globais = [
+                    3*id_n1, 3*id_n1+1, 3*id_n1+2, # Nó 1: ux, uy, theta
+                    3*id_n2, 3*id_n2+1, 3*id_n2+2  # Nó 2: ux, uy, theta
+                ]
+                # Soma elemento por elemento nas posições globais
+                for i in range(6):
+                    for j in range(6):
+                        K_g[gdl_globais[i], gdl_globais[j]] += ke[i, j]
+                        M_g[gdl_globais[i], gdl_globais[j]] += me[i, j]
+                        
+            elif isinstance(elem, Trelica):
+                gdl_globais = [
+                    3*id_n1, 3*id_n1+1, # Nó 1: ux, uy
+                    3*id_n2, 3*id_n2+1  # Nó 2: ux, uy
+                ]
+                # Soma elemento por elemento nas posições globais correspondentes
+                for i in range(4):
+                    for j in range(4):
+                        K_g[gdl_globais[i], gdl_globais[j]] += ke[i, j]
+                        M_g[gdl_globais[i], gdl_globais[j]] += me[i, j]
+       
+        salvar_matriz(K_g, results_dir, nome_base="Rigidez_Global")
+        plt.figure(figsize=(6, 6))
+        plt.spy(K_g, markersize=1)
+        plt.title("Perfil de Esparsidade da Matriz K")
+        salvar_grafico(plt, results_dir, "Esparsidade_K.png")
+        salvar_matriz(M_g, results_dir, nome_base="Massa_Global")
+        plt.figure(figsize=(6, 6))
+        plt.spy(M_g, markersize=1)
+        plt.title("Perfil de Esparsidade da Matriz M")
+        salvar_grafico(plt, results_dir, "Esparsidade_M.png")               
+        return K_g, M_g
 
 def main():
     estrutura = Estrutura()
-    estrutura.plot_estrutura()
+    #estrutura.plot_estrutura()
 
 if __name__ == "__main__":    
     main()
